ed1bae80197349853213d1bc5592c71ab7592930
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-06-17 18:15:15 +0100

    Add delete route

--- app.py
+++ app.py
@@ -24,10 +24,27 @@ def add():
     db.session.commit()
     return redirect(url_for("index"))
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
 
 if __name__ == "__main__":
     db.create_all()
